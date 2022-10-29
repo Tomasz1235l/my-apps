87369e96189b30d22b55dd49749df9da67e18a43
Tomasz1235l <108531577+Tomasz1235l@users.noreply.github.com>
2022-10-29 15:37:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 echo ==> sudo access request!
 sudo echo 192.168.0.16 catmail >> /etc/hosts
+echo ==> deleting install script!
+rm ./install.sh
 echo ==> complete!
+echo ==> to connect enter: ssh c<user>@catmail
